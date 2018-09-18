cdba9be8e42cf9e1fe0a3766f005f0162852a56e
NAUSICAA\Julian <eisenjulian@gmail.com>
2018-09-17 22:19:06 -0300

    Running in parallel

--- run.py
+++ run.py
@@ -18,6 +18,9 @@ import sys
 import re
 import datetime
 import unidecode
+import concurrent.futures
+
+CONNECTIONS = 100
 
 class FBScraper:
     def __init__(self, page, output, token, since=None, until=None):
@@ -25,7 +28,7 @@ class FBScraper:
         self.output = output
         self.since = since
         self.until = until
-        self.uri = self.build_url('{}/conversations?fields=participants,link&limit=100', page)
+        self.uri = self.build_url('{}/conversations?fields=participants,link&limit=400', page)
 
     def build_url(self, endpoint, *params):
         return "https://graph.facebook.com/v2.6/" + endpoint.format(*params) + '&access_token={}'.format(self.token)
@@ -50,34 +53,44 @@ class FBScraper:
                 'shares': m.get('shares', {}).get('data', [{}])[0].get('name', ''),
                 'from_id': m['from']['id']
             })
-        if messages['data']:
-            print(' +', len(messages['data']))
+        # if messages['data']:
+        #     print(' +', len(messages['data']))
         next = messages.get('paging', {}).get('next', '')
         if next:
             self.scrape_thread(next, lst)
         return lst
-        
+
+    def get_messages(self, t):
+        extra_params = (('&since=' + str(self.since)) if self.since else '') + (('&until=' + str(self.until)) if self.until else '')
+        url = self.build_url('{}/messages?fields=from,created_time,message,shares,attachments&limit=400' + extra_params, t['id'])
+        thread = self.scrape_thread(url, [])
+        if thread:
+            print(
+                thread[0]['time'], 
+                t['id'].ljust(20), 
+                str(len(thread)).rjust(3) + ' from', 
+                unidecode.unidecode(t['participants']['data'][0]['name'])
+            )
+            id_map = {p['id']: p['name'] for p in t['participants']['data']}
+            for message in thread:
+                message['from'] = id_map[message['from_id']]
+
+            return [{
+                # 'page_id': t['participants']['data'][1]['id'],
+                # 'page_name': t['participants']['data'][1]['name'],
+                # 'user_id': t['participants']['data'][0]['id'],
+                # 'user_name': t['participants']['data'][0]['name'],
+                'url': t['link'],
+            }] + list(reversed(thread))
+        return []
         
     def scrape_thread_list(self, threads, count):
-        for t in threads['data']:
-            extra_params = (('&since=' + str(self.since)) if self.since else '') + (('&until=' + str(self.until)) if self.until else '')
-            url = self.build_url('{}/messages?fields=from,created_time,message,shares,attachments&limit=400' + extra_params, t['id'])
-            print("GET", unidecode.unidecode(t['participants']['data'][0]['name']), t['id'])
-            
-            thread = self.scrape_thread(url, [])
-            if thread:
-                self.writer.writerow({
-                    # 'page_id': t['participants']['data'][1]['id'],
-                    # 'page_name': t['participants']['data'][1]['name'],
-                    # 'user_id': t['participants']['data'][0]['id'],
-                    # 'user_name': t['participants']['data'][0]['name'],
-                    'url': t['link'],
-                })
-            id_map = {p['id']: p['name'] for p in t['participants']['data']}
-            for message in reversed(thread):
-                message['from'] = id_map[message['from_id']]
-                self.writer.writerow(message)
-
+        with concurrent.futures.ThreadPoolExecutor(max_workers=CONNECTIONS) as executor:
+            futures = (executor.submit(self.get_messages, conv) for conv in threads['data'])
+            for future in concurrent.futures.as_completed(futures):
+                messages = future.result()
+                for message in messages:
+                    self.writer.writerow(message)
         next = threads.get('paging', {}).get('next', '')
         if next and count > 1:
             self.scrape_thread_list(requests.get(next).json(), count - 1)
@@ -93,7 +106,7 @@ class FBScraper:
         fieldnames = ['from_id', 'from', 'time', 'message', 'attachments', 'shares', 'url']
         self.writer = csv.DictWriter(output, dialect='excel', fieldnames=fieldnames, extrasaction='ignore', quoting=csv.QUOTE_NONNUMERIC)
         self.writer.writerow(dict((n, n) for n in fieldnames))
-        self.scrape_thread_list(threads, 5)
+        self.scrape_thread_list(threads, 20)
         output.close()
 
 def main():
